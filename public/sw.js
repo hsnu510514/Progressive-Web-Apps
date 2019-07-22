41274631410380f2f2c18722d88137841257b08e
Sean <hsnu510514@gmail.com>
2019-07-22 14:16:50 +0800

    beforeinstallallprompt

--- public/sw.js
+++ public/sw.js
@@ -7,5 +7,5 @@ self.addEventListener('activate', function(event){
 });
 self.addEventListener('fetch', function(event){
   console.log('[Service Worker] Fetching something ...', event);
-  event.respondWith(null);
+  // event.respondWith(null);
 });
