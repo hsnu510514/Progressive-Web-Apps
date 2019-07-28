5c90c0364c5d0ac0ca22e95cc6d8878112d20558
Sean <hsnu510514@gmail.com>
2019-07-28 09:54:44 +0800

    rwd

--- public/src/js/utility.js
+++ public/src/js/utility.js
@@ -38,3 +38,16 @@ function clearAllData(st) {
       return tx.complete;
     })
 }
+
+function deleteItemFromData(st, id) {
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.delete(id);
+      return tx.complete;
+    })
+    .then(function() {
+      console.log('Item deleted');
+    });
+}
