21758a8d7d5db66044ecfed265925794b292e8a5
Sean <hsnu510514@gmail.com>
2019-07-30 09:03:17 +0800

    Notification

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
-var cors = require('cors')({origin: true});
+var cors = require('cors')({ origin: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -12,19 +12,19 @@ admin.initializeApp({
   databaseURL: "https://pwagram0719.firebaseio.com"
 });
 
-exports.storePostData = functions.https.onRequest(function(request, response) {
- cors(request, response, function() {
-   admin.database().ref('posts').push({
-     id: request.body.id,
-     title: request.body.title,
-     location: request.body.location,
-     image: request.body.image
-   })
-   .then(function() {
-     response.status(201).json({message: 'Data stored', id: request.body.id});
-   })
-   .catch(function(err) {
-     response.status(500).json({error: err});
-   });
- });
+exports.storePostData = functions.https.onRequest(function (request, response) {
+  cors(request, response, function () {
+    admin.database().ref('posts').push({
+      id: request.body.id,
+      title: request.body.title,
+      location: request.body.location,
+      image: request.body.image
+    })
+      .then(function () {
+        response.status(201).json({ message: 'Data stored', id: request.body.id });
+      })
+      .catch(function (err) {
+        response.status(500).json({ error: err });
+      });
+  });
 });
