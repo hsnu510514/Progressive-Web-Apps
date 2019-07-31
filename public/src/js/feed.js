c5c4e645a3785f0faa88b9bb51b5e13693de9544
Sean <hsnu510514@gmail.com>
2019-07-31 10:05:01 +0800

    polyfill

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -12,7 +12,23 @@ var imagePicker = document.querySelector('#iamge-picker');
 var imagePickerArea = document.querySelector('#pick-image');
 
 function initializeMedia() {
-  
+  if (!('mdeiaDevices' in navigator)) {
+    navigator.mediaDevices = {};
+  }
+
+  if (!('getUserMedia' in navigator.mediaDevices)) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      if(!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented!'));
+      }
+
+      return new Promise(function(resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject)
+      })
+    }
+  }
 }
 
 
